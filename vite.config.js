a59764edd6305f4efe02cea70922aae6aa916815
Giddy-K <gideonkipamet@gmail.com>
2025-07-08 22:32:37 +0300

    Added vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/, // Treat all .js files in /src as JSX
-  },
-  server: {
-    port: 3000,
-  },
-})
+});
