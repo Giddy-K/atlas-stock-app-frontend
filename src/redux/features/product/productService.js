5d9f124aeb902a7da7755abcb4d65e37c5f7bbd2
Giddy-K <gideonkipamet@gmail.com>
2025-08-19 15:44:00 +0300

    WIP: upgrade react and local changes

--- src/redux/features/product/productService.js
+++ src/redux/features/product/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const API_URL = `${BACKEND_URL}/api/products/`;
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const validateEmail = (email) => {
   return email.match(
